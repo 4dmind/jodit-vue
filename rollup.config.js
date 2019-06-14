7de8aae99548315be72f26733ff09c8e45c2ab59
Karol Chojnacki <karol.chojnacki@mobyte.pl>
2019-06-14 09:10:36 +0200

    Fixing packages.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import vue from 'rollup-plugin-vue'
+import commonjs from 'rollup-plugin-commonjs';
 import buble from 'rollup-plugin-buble'
 import uglify from 'rollup-plugin-uglify-es'
 
@@ -18,6 +19,7 @@ export default {
       css: true,
       compileTemplate: true
     }),
+    commonjs(),
     buble({
       objectAssign: 'Object.assign'
     }),
